e188787cda35f95adb9e6e75ced3de0e7b7e9854
simone <simone.zienna@gmail.com>
2017-12-14 12:15:12 -0800

    Initial commit

---
+++ app/src/main/java/itp341/zienna/simone/pushup/MainActivity.java
@@ -0,0 +1,17 @@
+package itp341.zienna.simone.pushup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private void TestUpload() {
+        int i = 1 + 1;
+    }
+}
