a29c6397012c7c5e60c5d40ff9b8eab9b0d4bd32
simone <simone.zienna@gmail.com>
2017-12-14 12:24:30 -0800

    Modified MainActivity Added New File TestAddFile.java

--- app/src/main/java/itp341/zienna/simone/pushup/MainActivity.java
+++ app/src/main/java/itp341/zienna/simone/pushup/MainActivity.java
@@ -13,5 +13,9 @@ public class MainActivity extends AppCompatActivity {
 
     private void TestUpload() {
         int i = 1 + 1;
+
+        int ii = 2 + 2;
+
+        int iii = i + ii;
     }
 }
---
+++ app/src/main/java/itp341/zienna/simone/pushup/TestAddFile.java
@@ -0,0 +1,8 @@
+package itp341.zienna.simone.pushup;
+
+/**
+ * Created by simon on 12/14/2017.
+ */
+
+public class TestAddFile {
+}
